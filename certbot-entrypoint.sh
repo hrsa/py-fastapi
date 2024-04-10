fe3e5d0a52041deb354c249119785c687590b466
Anton Cherednichenko <anton@anton.eco>
2024-04-10 23:34:17 +0200

    removed staging flag for certbot-entrypoint.sh

--- certbot-entrypoint.sh
+++ certbot-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Obtain the certificate if it does not exist
 if [ ! -e "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ]; then
-  certbot certonly --webroot --webroot-path=/var/lib/letsencrypt --email $EMAIL --agree-tos --no-eff-email --staging -d $DOMAIN -d www.$DOMAIN
+  certbot certonly --webroot --webroot-path=/var/lib/letsencrypt --email $EMAIL --agree-tos --no-eff-email -d $DOMAIN -d www.$DOMAIN
 fi
 
 # Run a background job that tries to renew the certificate daily
